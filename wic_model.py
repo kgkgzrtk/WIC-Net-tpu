81f6597979880149dbaf55516a08e3bd3a2e0524
kgkgzrtk <r111331@g.kobe-kosen.ac.jp>
2019-01-29 12:26:44 +0900

    Update : Resize -> Deconv at upsampling of first layer in Generator

--- wic_model.py
+++ wic_model.py
@@ -108,12 +108,13 @@ def _res_block_down(x, out_dim, is_training, scope='res_down'):
         return c_s + x
 
 
-def _res_block_up(x, out_dim, is_training, scope='res_up'):
+def _res_block_up(x, out_dim, is_training, first=False, scope='res_up'):
     with tf.variable_scope(scope):
-        up_x = _upsampling(x, name='s_up')
-        c_s = _conv2d(up_x, out_dim, 1, 1, name='s_c')
+        c_s = _upsampling(x, name='s_up', mode=('deconv' if first else 'bi' ))
+        c_s = _conv2d(c_s, out_dim, 1, 1, name='s_c')
         #x = tf.layers.dropout(x, rate=0.3, training=is_training)
         x = _leaky_relu(_batch_norm(up_x, is_training, name='bn1'))
+        x = _upsampling(x, name='up', mode=('deconv' if first else 'bi' ))
         x = _conv2d(x, out_dim, 3, 1, name='c1')
         x = _leaky_relu(_batch_norm(x, is_training, name='bn2'))
         x = _conv2d(x, out_dim, 3, 1, name='c2')
@@ -125,7 +126,7 @@ def discriminator(x, a, is_training=True, scope='Discriminator'):
         dis_dim = 64
         feat_li = []
         for i in range(5):
-            if i<4: x = tf.layers.dropout(x, rate=0.5, training=is_training)
+            #if i<4: x = tf.layers.dropout(x, rate=0.5, training=is_training)
             x = _res_block_down(x, dis_dim*(2**i), is_training, scope='b_down_'+str(i))
             feat_li.append(x)
         x_feat = _leaky_relu(x)
@@ -142,7 +143,7 @@ def generator(x, is_training=True, scope='Generator'):
         x = _dense(x, 4*4*ch, name='fc')
         x = tf.reshape(x, [-1, 4, 4, ch])
         for i in range(5):
-            x = _res_block_up(x, ch//2, is_training, scope='b_up_'+str(i))
+            x = _res_block_up(x, ch//2, is_training, fisrt=(i==0), scope='b_up_'+str(i))
             ch = ch//2
         x = _leaky_relu(_batch_norm(x, is_training, name='bn'))
         x = _conv2d(x, 3, 3, 1, use_bias=True, name='final_c')
